24dbff425bcbe1e8717f3d1d844e6efe8e1f7c07
Alex. Z <azavgo@yahoo.com>
2022-12-09 20:02:11 +1100

    Sample free wav files to test the program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+
+//TODO: Load a wav file and retrieve its parameters such as audio length in seconds, number of channels, etc.
+//TODO: Write these parameters into a structure
+//TODO: Based on the parameters of the loaded wav file, create a picture
